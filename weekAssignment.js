5635126b0a66da923b9c84dff11ba866eaf8ea01
Prabesh Magar <Prabesh Magar>
2025-01-15 08:32:21 +0545

    New project added

--- weekAssignment.js
+++ weekAssignment.js
@@ -1,26 +1,67 @@
-let form = document.querySelector("form");
-let taskList = document.querySelector(".taskList")
 
-function addTask(event){
-    event.preventDefault()
-    let inputMess = document.querySelector("input").value;
+    let form = document.getElementById("form-submit");
+    let btn = document.getElementById("addBtn");
+    let task = document.getElementById("task");
+    let errorMess = document.getElementById("error-message");
+    let taskList = document.getElementById("taskList");
+    let count = document.getElementById("taskCount");
 
-    if(inputMess === ''){
-        alert ("wrong request")
-        // form.disabled = true;
+    let taskcounter = 0;
+    let taskSet = new Set();
+
+    function updatedTaskCount(){
+        count.textContent = `Task Remaining: ${taskcounter}`
     }
 
-    let demo = document.createElement('li');
+    form.addEventListener("submit", function (event){
+        event.preventDefault();
+        let inputValue = task.value.trim();
 
-    demo.innerHTML = `
-    <li>${inputMess} <button onclick = delelteItems(this) >Delete</button></li>
-    `
-    taskList.append(demo);
 
-    document.querySelector("input").value = '';
-}
+        if(inputValue === ""){
+            errorMess.textContent = "!! Please enter a task !!";
+            return;
+        }
 
-function delelteItems(button){
-    let demo = button.closest('li')
-    demo.remove();
-}
+        if(taskSet.has(inputValue)){
+            errorMess.textContent = "!! Sorry, Task already taken !!"
+            return;
+        }
+
+        errorMess.textContent = "";
+
+        let li = document.createElement('li');
+
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("checkbox");
+        checkbox.addEventListener("change", function(){
+            li.classList.toggle("completed", checkbox.checked);
+        })
+
+        let deletBtn = document.createElement("button");
+        deletBtn.textContent = "Delete";
+        deletBtn.classList.add("delete-btn");
+        deletBtn.addEventListener("click", function(){
+            li.remove();
+            taskSet.delete(inputValue);
+            taskcounter--;
+            updatedTaskCount();
+        })
+
+        let todoList = document.createElement("span");
+        todoList.textContent = inputValue;
+
+        li.appendChild(checkbox);
+        li.appendChild(todoList);
+        li.appendChild(deletBtn);
+
+        taskList.appendChild(li);
+        taskSet.add(inputValue)
+
+        task.value = "";
+
+        taskcounter++;
+        updatedTaskCount();
+    })
+    updatedTaskCount()
